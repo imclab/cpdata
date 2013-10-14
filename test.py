3ada6095c657104e747af58a8522d0e48edea449
Tom Baker <tom@tombaker.org>
2013-10-14 14:50:24 -0400

    Create cpdata.py with two methods.

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ import cpdata
 class CPDataTest(unittest.TestCase):
 
     def test_size(self):
-        g = cpdata.Graph("http://aims.fao.org/aos/geopolitical.owl")
-        self.assertEquals(g.size(), 42)
+        g = cpdata.Summarizer("http://aims.fao.org/aos/geopolitical.owl")
+        self.assertEquals(g.size(), 23780)
